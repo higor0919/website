dc9def8220d007c05bf84e296f5e17db28b7c915
higor0919 <124521305+higor0919@users.noreply.github.com>
2025-04-23 16:37:35 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@builder.io/react', '@builder.io/sdk']
+};
+
+module.exports = nextConfig;
